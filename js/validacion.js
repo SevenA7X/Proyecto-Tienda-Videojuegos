33b1547ebca9c23117709be0382adfe5c63685fc
Nicolas Contador <ncontadorvega@gmail.com>
2026-09-06 01:03:42 -0300

    Se implemento el inicio de sesion de admin en validacion.js y se creo la vista de admin

--- js/validacion.js
+++ js/validacion.js
@@ -1,11 +1,37 @@
-const formulario = document.querySelector('#formularioContacto');
+const formularioLogin = document.querySelector("#formularioContacto");
 
+if (formularioLogin) {
+    formularioLogin.addEventListener("submit", function(evento) {
+        // Detenemos el envío tradicional del formulario para procesarlo con JavaScript
+        evento.preventDefault();
+        evento.stopPropagation();
 
-formulario.addEventListener('submit', function (event) {
-    if (!formulario.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
-    }
+        // Activamos las validaciones visuales rojas/verdes de Bootstrap
+        formularioLogin.classList.add('was-validated');
 
-    formulario.classList.add('was-validated');
-}, false);
+        // Si los campos requeridos están llenos (checkValidity es true)
+        if (formularioLogin.checkValidity()) {
+            const correo = document.querySelector("#correo").value;
+            const password = document.querySelector("#password").value;
+
+            // Validamos las credenciales maestras de tu mockup
+            if (correo === "ejemplo@correo.cl" && password === "123456") {
+                
+                // Creamos el objeto del usuario con el rol de Administrador
+                const usuario = {
+                    correo: correo,
+                    rol: "Administrador"
+                };
+                
+                // Guardamos la sesión en el almacenamiento local para que admin.html la lea
+                localStorage.setItem("sesionActiva", JSON.stringify(usuario));
+                
+                // Abrimos la puerta al panel
+                window.location.href = "admin.html";
+            } else {
+                // Si alguien intenta adivinar tus datos, lo detendremos aquí
+                alert("Las credenciales son incorrectas, inténtalo de nuevo.");
+            }
+        }
+    });
+}
